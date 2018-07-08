9c77b721c1dd1c48fb6982357c777eba2a3870f5
Kevin <kev.giguere.l@gmail.com>
2018-07-08 18:30:56 -0400

    add afterModuleUpdate function

--- index.js
+++ index.js
@@ -50,6 +50,43 @@ AutoCommit.addAndCommit = function(msg){
   });
 }
 
+AutoCommit.afterModuleUpdate = function(opt, cb){
+  return AutoCommit(opt, function(){
+    return exec('npm update --json', {}).then(function(out){
+      res = JSON.parse(out.stdout)
+      updates = AutoCommit.filterModuleUpdates(res.updated, opt.filter);
+      if(updates.length > 0){
+        return asyncDone(cb).then(function(res){
+          return 'Update ' + AutoCommit.englishEnumeration(updates.map(function(u){
+            return u.name;
+          }));
+        });
+      }
+    });
+  });
+}
+
+AutoCommit.filterModuleUpdates = function(updates, filter){
+  return updates.filter(function(u){
+    return u.version !== u.previousVersion && (
+        typeof filter == null ||
+        (typeof filter === "function" && filter(u)) ||
+        (typeof ilter.test === "function" && filter.test(u.name))
+      )
+  });
+} 
+
+
+AutoCommit.englishEnumeration = function(items){
+  if(items.length == 1){
+    return items[0];
+  }else if(items.length > 1){
+    var last = items[items.length-1]
+    return items.slice(0,items.length-1).join(', ') + ' and ' + last
+  }
+  return null;
+}
+
 
 
 module.exports = AutoCommit
